46b2948f888d818f7c54fb4b0eb2c2bd2e54c7c0
Alex Chan <alex@alexwlchan.net>
2016-08-26 12:04:55 +0100

    actually build the site

--- pre-flight-checks.sh
+++ pre-flight-checks.sh
@@ -50,6 +50,7 @@ fi
 
 echo " *** Checking that conference name is spelt correctly"
 
+python manage.py buildsite
 grep -i -e "Pycon UK" -e "PyconUK" --line-number --recursive --include "*.html" output | grep -v https://twitter.com/PyConUK | grep -v "PyCon UK"
 
 if [[ $? -eq 0 ]]; then
