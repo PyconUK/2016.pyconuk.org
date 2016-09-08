9a185232e42bc81151711176c7ac1bf346f6c14b
Peter Inglesby <peter.inglesby@gmail.com>
2016-09-08 18:02:34 +0100

    Add index pages for sessions, speakers, sponsors
    This is mostly for admin purposes

--- pyconuk/urls.py
+++ pyconuk/urls.py
@@ -9,8 +9,11 @@ urlpatterns = [
     url(r'^news/$', views.news_items_view, name='news_items'),
     url(r'^news/(?P<datestamp>\d+)-(?P<key>[\w-]+)/$', views.news_item_view, name='news_item'),
     url(r'^(?P<session_type>talks|workshops|keynotes)/(?P<slug>[\w-]+)/$', views.session_view, name='session'),
+    url(r'^sessions/$', views.sessions_view, name='sessions'),
     url(r'^speakers/(?P<key>[\w-]+)/$', views.speaker_view, name='speaker'),
+    url(r'^speakers/$', views.speakers_view, name='speakers'),
     url(r'^sponsors/(?P<key>[\w-]+)/$', views.sponsor_view, name='sponsor'),
+    url(r'^sponsors/$', views.sponsors_view, name='sponsors'),
     url(r'^$', views.page_view, name='index'),
     url(r'^(?P<key>.*?)/$', views.page_view, name='page'),
 ]
